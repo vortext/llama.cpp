efbef6ec0b777f10cae77bc069176c251c214473
Joel Kuiper <me@joelkuiper.eu>
2023-08-29 16:25:37 +0200

    Up context limit in main

--- examples/main/main.cpp
+++ examples/main/main.cpp
@@ -90,7 +90,7 @@ int main(int argc, char ** argv) {
         fprintf(stderr, "%s: warning: scaling RoPE frequency by %g (default 1.0)\n", __func__, params.rope_freq_scale);
     }
 
-    if (params.n_ctx > 2048) {
+    if (params.n_ctx > 4096) {
         // TODO: determine the actual max context of the model (e.g. 4096 for LLaMA v2) and use that instead of 2048
         fprintf(stderr, "%s: warning: base model only supports context sizes no greater than 2048 tokens (%d specified)\n", __func__, params.n_ctx);
     } else if (params.n_ctx < 8) {
